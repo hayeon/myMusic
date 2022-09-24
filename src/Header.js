7d1bca44f494388e19c1d38b5b260e74a3a060d4
hayeon <hayun4475@gmail.com>
2022-09-24 23:55:45 +0900

    header 수정

--- src/Header.js
+++ src/Header.js
@@ -1,8 +1,5 @@
 import styled from "styled-components";
-import { motion, useAnimation, useViewportScroll } from "framer-motion";
-import { Link, useHistory, useRouteMatch } from "react-router-dom";
-import { useEffect, useState } from "react";
-import { useForm } from "react-hook-form";
+import { Link} from "react-router-dom";
 
 const Nav = styled.nav`
   display: flex;
@@ -48,37 +45,14 @@ const Item = styled.li`
 `;
 
 function Header() {
-  // //useRouteMatch를 통해 현재 위치가 어디인지
-  // //만약에 "/"에 있다면 , HoverMatch가 위치해야함
-  // const homeMatch = useRouteMatch("/");
-  // const searhMatch = useRouteMatch("/artists");
-  // const serachBarAnimation = useAnimation();
-  // const navAnimation = useAnimation();
-  // const { scrollY } = useViewportScroll();
-  // //검색바 열었을 때 애니메이션
-
-  // useEffect(() => {
-  //   //헤더 흐려지는 애니메이션
-  //   scrollY.onChange(() => {
-  //     if (scrollY.get() > 10) {
-  //       navAnimation.start("scroll");
-  //     } else {
-  //       navAnimation.start("top");
-  //     }
-  //   });
-  // }, [scrollY, navAnimation]);
-  const [token, setToken] = useState("");
-  
-  
-  const logout = () => {
-    setToken("");
-    window.localStorage.removeItem("token");
-    console.log(token);
-  };
+  //useRouteMatch를 통해 현재 위치가 어디인지
+  //만약에 "/"에 있다면 , HoverMatch가 위치해야함
 
   return (
+    
     <Nav>
-      <HeaderIn>
+      <Link to="/searchartist" style={{ textDecoration: "none" }}>아티스트 찾기 </Link>
+      {/* <HeaderIn>
         <Items>
           <Item>
             <h1> <Link to="" style={{ textDecoration: "none" }}>Home</Link></h1>
@@ -90,7 +64,7 @@ function Header() {
            로그아웃
           </button>
         </Items>
-      </HeaderIn>
+      </HeaderIn> */}
     </Nav>
   );
 }
