ffa6c804df2219919bcdaebc5398f47538e794dc
hayeon <hayun4475@gmail.com>
2022-09-26 23:55:28 +0900

    box plus

--- src/routes/Slider.js
+++ src/routes/Slider.js
@@ -32,8 +32,21 @@ const Box = styled(motion.div)`
   box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
 `;
 
+const Boxes = styled(motion.div)`
+  width: 400px;
+  height: 200px;
+  background-color: rgba(255, 255, 255, 1);
+  border-radius: 40px;
+  position: absolute;
+  top: 100px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-size: 28px;
+  box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
+`;
 const SlideAnimation = {
-  //슬라이드 애니메이션
+  //슬라이드 애니메이션 추가
   hidden: { x: window.outerWidth + 5 },
   visible: { x: 0 },
   exit: { x: -window.outerWidth - 5 },
