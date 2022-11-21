db98712891f6cc283145019f2109c3f7818a603e
hayeon <hayun4475@gmail.com>
2022-11-21 22:45:31 +0900

    Top Tracks 50

--- src/routes/Searchartist.js
+++ src/routes/Searchartist.js
@@ -2,32 +2,28 @@ import axios from "axios";
 import { useState } from "react";
 import styled from "styled-components";
 
-const Background = styled.div `
-align-items: center;
-position: relative;
-height: 200px;
+const Background = styled.div`
+  margin-top: 10vh;
+  align-items: center;
+  height: 100%;
 `;
 
-const MainText = styled.div`
-position: absolute;
-font-size: 30px;
-text-align: center;
-background-color: red;
-top:50%;
-left: 50%;
+const MainText = styled.h1`
+  font-size: 30px;
+  text-align: center;
+  background-color: red;
+  top: 50%;
+  left: 50%;
 `;
-
-
-
 
 function Searchartist() {
   /*검색어를 입력하면 변수에 저장*/
   const [searchKey, setSearchKey] = useState("");
   const [artists, setArtists] = useState([]);
-  const token = window.localStorage.getItem('token');
+  const token = window.localStorage.getItem("token");
   const searchArtistsBar = async (e) => {
-    const { data } = await axios.get("https://api.spotify.com/v1/search", 
-    {  headers: {
+    const { data } = await axios.get("https://api.spotify.com/v1/search", {
+      headers: {
         Authorization: `Bearer ${token}`,
       },
       params: {
@@ -36,9 +32,8 @@ function Searchartist() {
       },
     });
     setArtists(data.artists.items);
-    console.log(artists);
+    artists ? console.log(artists) : console.log("데이터가 없어요");
   };
-
 
   const renderArtists = () => {
     return artists.map((artist) => (
@@ -46,21 +41,22 @@ function Searchartist() {
         {artist.images.length ? (
           <img width={"100%"} src={artist.images[0].url} alt="" />
         ) : (
-          <div>검색된 결과가 없습니다. 다시 입력해주세요</div>
+          <MainText>
+            검색된 결과가 없습니다. 다시 입력해주세요 {artist.name}
+          </MainText>
         )}
-        {artist.name}
       </div>
     ));
   };
 
   return (
-    <>
+    <Background>
       <form onSubmit={searchArtistsBar}>
         <input type="text" onChange={(e) => setSearchKey(e.target.value)} />
         <button type="submit">Search</button>
       </form>
       {renderArtists()}
-    </>
+    </Background>
   );
 }
 
